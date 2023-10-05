ff333c2d0a0505a207d59e9db67a1ae17d710921
Martin Braun <martinbraun123@aol.com>
2023-10-05 14:44:45 +0200

    Update install_dependencies.sh

--- environments_encrypted/prod/prod-swarm/install_dependencies.sh
+++ environments_encrypted/prod/prod-swarm/install_dependencies.sh
@@ -5,5 +5,5 @@ source ../../prod-venv/bin/activate
 pip3 install ansible==6.5.0
 pip3 install hcloud==1.18.0
 pip3 install yq
-pip3 install https://github.com/neuroforgede/docker-stack-deploy/archive/refs/tags/0.2.3.zip
+pip3 install https://github.com/neuroforgede/docker-stack-deploy/archive/refs/tags/0.2.12.zip
 ansible-galaxy collection install hetzner.hcloud:==1.9.1
